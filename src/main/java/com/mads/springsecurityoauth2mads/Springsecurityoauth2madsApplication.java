03810fe9f2cf8d2228228e734597f9ccdf0edbbd
Santiago Sáez Caselles <ssc64@mscloud.ua.es>
2022-12-26 12:08:39 +0100

    first commit

---
+++ src/main/java/com/mads/springsecurityoauth2mads/Springsecurityoauth2madsApplication.java
@@ -0,0 +1,13 @@
+package com.mads.springsecurityoauth2mads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurityoauth2madsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecurityoauth2madsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mads/springsecurityoauth2mads/Springsecurityoauth2madsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mads.springsecurityoauth2mads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecurityoauth2madsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
